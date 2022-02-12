7bfd86cf575edc1cff82365b042471d00a6776a4
broizter <39500356+broizter@users.noreply.github.com>
2022-02-12 17:47:41 +0100

    Razorscale: Revert P2 detection that didn't work

--- DBM-Ulduar/localization.en.lua
+++ DBM-Ulduar/localization.en.lua
@@ -73,7 +73,7 @@ L:SetMiscLocalization({
 	YellAir				= "Give us a moment to prepare to build the turrets.",
 	YellAir2			= "Fires out! Let's rebuild those turrets!",
 	YellGround			= "Move quickly! She won't remain grounded for long!",
-	EmotePhase2			= "Razorscale is grounded permanently!"
+	EmotePhase2			= "%%s grounded permanently!"
 })
 
 ----------------------------
