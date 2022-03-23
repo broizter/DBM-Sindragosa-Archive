3f8b615f645cbccbe5e9eb10e9b1cfb0e1cfaa99
broizter <39500356+broizter@users.noreply.github.com>
2022-03-23 12:23:32 +0100

    Koralon: Fixes

--- DBM-VoA/Koralon.lua
+++ DBM-VoA/Koralon.lua
@@ -7,8 +7,8 @@ mod:SetCreatureID(35013)
 mod:RegisterCombat("combat")
 
 mod:RegisterEventsInCombat(
-	"SPELL_CAST_START 67328 66665 66725 68161",
-	"SPELL_AURA_APPLIED 66684 67332 66721",
+	"SPELL_CAST_START 67328 66665",
+	"SPELL_AURA_APPLIED 66684 67332 66721 66725 68161",
 	"SPELL_AURA_APPLIED_DOSE 66721"
 )
 
@@ -19,7 +19,7 @@ local warnBurningFury		= mod:NewStackAnnounce(66721, 2, nil, "Tank|Healer")
 
 local specWarnCinder		= mod:NewSpecialWarningMove(66684, nil, nil, nil, 1, 2)
 
-local timerNextMeteor		= mod:NewNextTimer(47, 66725, nil, nil, nil, 5, nil, DBM_CORE_L.TANK_ICON)
+local timerNextMeteor		= mod:NewNextTimer(45, 66725, nil, nil, nil, 5, nil, DBM_CORE_L.TANK_ICON)
 local timerNextBurningFury	= mod:NewNextTimer(20, 66721, nil, nil, nil, 5, nil, DBM_CORE_L.TANK_ICON..DBM_CORE_L.HEALER_ICON)
 local timerBreath			= mod:NewBuffActiveTimer(4.5, 66665, nil, nil, nil, 2)
 local timerBreathCD			= mod:NewCDTimer(45, 66665, nil, nil, nil, 2)--Seems to variate, but 45sec cooldown looks like a good testing number to start.
@@ -28,9 +28,9 @@ local timerKoralonEnrage	= mod:NewBerserkTimer(300, nil, "KoralonEnrage")
 
 function mod:OnCombatStart(delay)
 	timerKoralonEnrage:Start(-delay)
-	timerNextMeteor:Start(-delay)
+	timerNextMeteor:Start(30-delay)
 	timerBreathCD:Start(12-delay)
-	warnMeteorSoon:Schedule(42-delay)
+	warnMeteorSoon:Schedule(25-delay)
 	timerNextBurningFury:Start()
 end
 
@@ -39,10 +39,6 @@ function mod:SPELL_CAST_START(args)
 		warnBreath:Show()
 		timerBreath:Start()
 		timerBreathCD:Start()
-	elseif args:IsSpellID(66725, 68161) then
-		warnMeteor:Show()
-		timerNextMeteor:Start()
-		warnMeteorSoon:Schedule(42)
 	end
 end
 
@@ -53,6 +49,10 @@ function mod:SPELL_AURA_APPLIED(args)
 	elseif args.spellId == 66721 then
 		warnBurningFury:Show(args.destName, args.amount or 1)
 		timerNextBurningFury:Start()
+	elseif args:IsSpellID(66725, 68161) then
+		warnMeteor:Show()
+		timerNextMeteor:Start()
+		warnMeteorSoon:Schedule(40)
 	end
 end
 mod.SPELL_AURA_APPLIED_DOSE = mod.SPELL_AURA_APPLIED
