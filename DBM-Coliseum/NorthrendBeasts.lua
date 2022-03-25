f88f60f1886212f2ab274c2978ac8db91e7d6906
broizter <39500356+broizter@users.noreply.github.com>
2022-03-25 22:29:47 +0100

    Beasts: Reduce spam from fire GTFO warning

--- DBM-Coliseum/NorthrendBeasts.lua
+++ DBM-Coliseum/NorthrendBeasts.lua
@@ -309,7 +309,7 @@ function mod:SPELL_CAST_SUCCESS(args)
 end
 
 function mod:SPELL_DAMAGE(_, _, _, destGUID, _, _, spellId, spellName)
-	if (spellId == 66320 or spellId == 67472 or spellId == 67473 or spellId == 67475 or spellId == 66317) and destGUID == UnitGUID("player") then	-- Fire Bomb (66317 is impact damage, not avoidable but leaving in because it still means earliest possible warning to move. Other 4 are tick damage from standing in it)
+	if (spellId == 66320 or spellId == 67472 or spellId == 67473 or spellId == 67475) and destGUID == UnitGUID("player") then  -- Fire Bomb
 		specWarnGTFO:Show(spellName)
 		specWarnGTFO:Play("runaway")
 	elseif (spellId == 66881 or spellId == 67638 or spellId == 67639 or spellId == 67640) and destGUID == UnitGUID("player") then	-- Slime Pool
